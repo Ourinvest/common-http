b1c5940829a86f688070acc75fe93ef5d5f5528e
bruno-mouazzem <97047250+bruno-mouazzem@users.noreply.github.com>
2022-06-21 15:27:04 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 requirements = ["fastapi",
                 "pydantic"]
 
-setup(name="customHttpResponse", version="0.0.1",
+setup(name="common_http", version="0.0.1",
       url='https://github.com/Ourinvest/common-http.git',
       packages=find_packages(),
       install_requires=requirements)
