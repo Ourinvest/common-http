d10c886cb0efe311cff05a55717f5eef8d635cff
bruno-mouazzem <97047250+bruno-mouazzem@users.noreply.github.com>
2022-06-21 15:12:56 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
+
 from setuptools import find_packages, setup
 
-requirements = [
-    "flask-restplus==0.13.0",
-    "flask==1.1.1",
-    "marshmallow==3.3.0",
-    "simplejson==3.17.0",
-    "werkzeug==0.16.0",
-]
+requirements = ["fastapi",
+                "pydantic"]
 
-setup(name="common-http", version="1.2.7", packages=find_packages(), install_requires=requirements)
+setup(name="customHttpResponse", version="0.0.1",
+      url='https://github.com/Ourinvest/common-http.git',
+      packages=find_packages(),
+      install_requires=requirements)
